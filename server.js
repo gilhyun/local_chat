03a084a54d30afb2e68ee0b7f90d6760172d0758
idoroz <dombi123@gmail.com>
2017-01-31 19:09:50 +0200

    init - no nodemon

---
+++ server.js
@@ -0,0 +1,14 @@
+console.log('server is starting...');
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000, listening);
+
+function listening() {
+    console.log('listening...');
+};
+
+app.use(express.static('src'));
+
+
